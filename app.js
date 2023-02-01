8839415cac41d0ea5f16c666c3eea42c0b259003
dianaveselkin <vess_di@mail.ru>
2023-02-01 23:02:59 +0300

    active page

---
+++ app.js
@@ -0,0 +1,18 @@
+function slidesPlugin(activesSlide = 3) {
+  const slides = document.querySelectorAll('.slide')
+  slides[activesSlide].classList.add('active')
+  for (const slide of slides) {
+    slide.addEventListener('click', ()=>{
+      clearActiveClasses()
+      slide.classList.add('active')
+    })
+  }
+  
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove('active')
+  
+    })
+  } 
+}
+slidesPlugin()
