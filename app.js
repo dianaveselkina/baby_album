6b58044fae0a63f4ac4212c0b1eed7455dfadddb
dianaveselkina <vess_di@mail.ru>
2023-07-21 21:32:28 +0300

    add foto

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-function slidesPlugin(activesSlide = 3) {
-  const slides = document.querySelectorAll('.slide')
-  slides[activesSlide].classList.add('active')
+function slidesPlugin(activesSlide = 5) {
+  const slides = document.querySelectorAll('.slide');
+  slides[activesSlide].classList.add('active');
   for (const slide of slides) {
-    slide.addEventListener('click', ()=>{
-      clearActiveClasses()
-      slide.classList.add('active')
-    })
+    slide.addEventListener('click', () => {
+      clearActiveClasses();
+      slide.classList.add('active');
+    });
   }
-  
+
   function clearActiveClasses() {
     slides.forEach((slide) => {
-      slide.classList.remove('active')
-  
-    })
-  } 
+      slide.classList.remove('active');
+    });
+  }
 }
-slidesPlugin()
+slidesPlugin();
